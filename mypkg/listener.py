8dc8e1420eb087df81e77e6bd88d296f1b9df13e
souzou <s24c1109hv@s.chibakoudai.jp>
2025-12-29 22:41:12 +0900

    tried creating it once

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,17 +1,38 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2025 souzouryoku00
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
+class Listener(Node):
+    def __init__(self):
+        super().__init__('listener')
+        # 'count_up' というトピックを購読
+        self.subscription = self.create_subscription(
+            Int16,
+            'count_up',
+            self.cb,
+            10
+        )
 
-rclpy.init()
-node = Node("listener")
+    def cb(self, msg):
+        # 受け取った数字を2倍にする計算
+        result = msg.data * 2
+        # 結果をログに出力
+        self.get_logger().info(f'Listen: {msg.data} -> Double: {result}')
 
+def main(args=None):
+    rclpy.init(args=args)
+    node = Listener()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
-
-
-def main():
-    pub = node.create_subscription(Int16, "countup", cb, 10)
-    rclpy.spin(node)
+if __name__ == '__main__':
+    main()
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,21 +1,35 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2025 souzouryoku00
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
+class Talker(Node):
+    def __init__(self):
+        super().__init__('talker')
+        self.publisher_ = self.create_publisher(Int16, 'count_up', 10)
+        self.n = 0
+        self.timer = self.create_timer(0.5, self.cb)
 
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publish: {msg.data}')
+        self.n += 1
 
-def cb():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
+def main(args=None):
+    rclpy.init(args=args)
+    node = Talker()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
-
-def main():
-    node.create_timer(0.5, cb)
-    rclpy.spin(node)
+if __name__ == '__main__':
+    main()
