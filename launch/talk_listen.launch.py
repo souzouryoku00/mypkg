71adb6ba3bb6ce7c571ef2a620f0a57afbf1a608
souzou <s24c1109hv@s.chibakoudai.jp>
2025-12-31 00:01:44 +0900

    add command

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -3,17 +3,38 @@ import launch.actions
 import launch.substitutions
 import launch_ros.actions
 
-
 def generate_launch_description():
+
+    symbol_arg = launch.actions.DeclareLaunchArgument(
+        'symbol',
+        default_value='+',
+        description='Calculation symbol'
+    )
+    
+    num_arg = launch.actions.DeclareLaunchArgument(
+        'num',
+        default_value='5',
+        description='Calculation number'
+    )
 
     talker = launch_ros.actions.Node(
         package='mypkg',
         executable='talker',
-        )
+        parameters=[{
+            'symbol': launch.substitutions.LaunchConfiguration('symbol'),
+            'num': launch.substitutions.LaunchConfiguration('num'),
+        }]
+    )
+
     listener = launch_ros.actions.Node(
         package='mypkg',
         executable='listener',
         output='screen'
-        )
+    )
 
-    return launch.LaunchDescription([talker, listener])
+    return launch.LaunchDescription([
+        symbol_arg,
+        num_arg,
+        talker,
+        listener,
+    ])
