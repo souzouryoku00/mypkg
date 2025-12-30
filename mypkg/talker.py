08c4493a1e8dbdc9d180c6be8bd66aae34484916
souzou <s24c1109hv@s.chibakoudai.jp>
2025-12-30 20:42:24 +0900

    Improved talker.py

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -4,32 +4,33 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import String
 
 class Talker(Node):
     def __init__(self):
         super().__init__('talker')
-        self.publisher_ = self.create_publisher(Int16, 'count_up', 10)
+        self.pub = self.create_publisher(String, 'topic', 10)
+        self.create_timer(1.0, self.cb)
         self.n = 0
-        self.timer = self.create_timer(0.5, self.cb)
 
     def cb(self):
-        msg = Int16()
-        msg.data = self.n
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'Publish: {msg.data}')
+        # 計算式を作成 (例: "0 + 5", "1 + 5"...)
+        expression = f"{self.n} + 5"
+        msg = String()
+        msg.data = expression
+        self.pub.publish(msg)
+        self.get_logger().info(f'Publish: "{expression}"')
         self.n += 1
 
-def main(args=None):
-    rclpy.init(args=args)
+def main():
+    rclpy.init()
     node = Talker()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
