cfe94c49d7dc7bbe1d2421cd0d2760d51b4871a4
souzou <s24c1109hv@s.chibakoudai.jp>
2025-12-30 21:45:42 +0900

    Add test item

--- test/test.bash
+++ test/test.bash
@@ -24,5 +24,18 @@ cat /tmp/mypkg.log | grep 'Listen:'
 cat /tmp/mypkg.log | grep 'Answer:'
 [ "$?" = "0" ] || ng "$LINENO"
 
+echo "後半"
+
+PYTHONUNBUFFERED=1 ros2 run mypkg listener > /tmp/mypkg_zero.log 2>&1 &
+LISTENER_PID=$! 
+
+sleep 2 
+timeout 5 ros2 run mypkg talker --ros-args -p symbol:="'/'" -p num:=0 > /dev/null 2>&1
+
+kill $LISTENER_PID
+
+cat /tmp/mypkg_zero.log | grep 'division by zero'
+[ "$?" = "0" ] || ng "$LINENO"
+
 [ "$res" = 0 ] && echo "OK"
 exit $res
